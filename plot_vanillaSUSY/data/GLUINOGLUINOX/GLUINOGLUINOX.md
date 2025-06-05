b09b20da9fb918d8b9eb015f510d6ad3c1ea5bbf
Lawrence Lee <lawrenceleejr@gmail.com>
2025-06-05 13:25:07 -0400

    finishing off adding the gluinos to the vanilla plot

--- plot_vanillaSUSY/data/GLUINOGLUINOX/GLUINOGLUINOX.md
+++ plot_vanillaSUSY/data/GLUINOGLUINOX/GLUINOGLUINOX.md
@@ -1,11 +1,11 @@
 | arXiv:1710.04901 | 2018 | ATLAS | https://www.hepdata.net/record/ins1630632 | x LLPs
-| arXiv:1710.11188 | 2018 | CMS   | https://www.hepdata.net/record/ins1633588 |
-| arXiv:1711.01901 | 2018 | ATLAS | https://www.hepdata.net/record/ins1634607 |
-| arXiv:1712.02118 | 2018 | ATLAS | https://www.hepdata.net/record/ins1641262 |
+| arXiv:1710.11188 | 2018 | CMS   | https://www.hepdata.net/record/ins1633588 | 🔸 (not yet in plot) - Not Full Run-2
+| arXiv:1711.01901 | 2018 | ATLAS | https://www.hepdata.net/record/ins1634607 | Not Full Run-2
+| arXiv:1712.02118 | 2018 | ATLAS | https://www.hepdata.net/record/ins1641262 | Not Full Run-2
 | arXiv:1712.02332 | 2018 | ATLAS | https://www.hepdata.net/record/ins1641270 | x LLPs
-| arXiv:1805.11381 | 2018 | ATLAS | https://www.hepdata.net/record/ins1675352 |
+| arXiv:1805.11381 | 2018 | ATLAS | https://www.hepdata.net/record/ins1675352 | Not Full Run-2
 | arXiv:1808.04095 | 2019 | ATLAS | https://www.hepdata.net/record/ins1686832 | x LLPs
-| arXiv:1808.06358 | 2019 | ATLAS | https://www.hepdata.net/record/ins1688943 |
+| arXiv:1808.06358 | 2019 | ATLAS | https://www.hepdata.net/record/ins1688943 | Not Full Run-2
 | arXiv:1811.07991 | 2019 | CMS   | https://www.hepdata.net/record/ins1704319 | x LLPs
 | arXiv:1902.01636 | 2019 | ATLAS | https://www.hepdata.net/record/ins1718558 | x LLPs
 | arXiv:1908.04722 | 2019 | CMS   | https://www.hepdata.net/record/ins1749379 | ✅
@@ -18,11 +18,11 @@
 | arXiv:2103.01290 | 2021 | CMS   | https://www.hepdata.net/record/ins1849522 | 🔸 (not yet in plot)
 | arXiv:2104.13474 | 2021 | CMS   | https://www.hepdata.net/record/ins1861146 | x LLPs
 | arXiv:2201.04206 | 2022 | CMS   | https://www.hepdata.net/record/ins2009652 | x Higgsinos
-| arXiv:2204.13072 | 2023 | ATLAS | https://www.hepdata.net/record/ins2072870 |
+| arXiv:2204.13072 | 2023 | ATLAS | https://www.hepdata.net/record/ins2072870 | 🔸 (not yet in plot)
 | arXiv:2205.06013 | 2023 | ATLAS | https://www.hepdata.net/record/ins2080541 | x LLPs
-| arXiv:2206.06012 | 2023 | ATLAS | https://www.hepdata.net/record/ins2094882 |
-| arXiv:2211.08028 | 2023 | ATLAS | https://www.hepdata.net/record/ins2182381 |
+| arXiv:2206.06012 | 2023 | ATLAS | https://www.hepdata.net/record/ins2094882 | 🔸 (not yet in plot)
+| arXiv:2211.08028 | 2023 | ATLAS | https://www.hepdata.net/record/ins2182381 | 🔸 (not yet in plot)
 | arXiv:2301.13866 | 2023 | ATLAS | https://www.hepdata.net/record/ins2628398 | x LLPs
-| arXiv:2307.01094 | 2024 | ATLAS | https://www.hepdata.net/record/ins2673888 |
+| arXiv:2307.01094 | 2024 | ATLAS | https://www.hepdata.net/record/ins2673888 | 🔸 (not yet in plot)
 | arXiv:2402.15804 | 2024 | CMS   | https://www.hepdata.net/record/ins2761908 | x LLPs
 | arXiv:2404.02992 | 2024 | CMS   | https://www.hepdata.net/record/ins2774247 | x RPV
--- plot_vanillaSUSY/plot.py
+++ plot_vanillaSUSY/plot.py
@@ -43,6 +43,74 @@ data_gg["arXiv_2010.14293"] = add_zero_endpoints(data_gg["arXiv_2010.14293"],(0,
 data_gg["arXiv_2101.01629"] = np.genfromtxt("data/GLUINOGLUINOX/arXiv_2101.01629/HEPData-ins1839446-v2-Exclusion_contour_1_(obs.).csv", delimiter=",", skip_header=9, names=["x","y"])
 data_gg["arXiv_2101.01629"] = add_zero_endpoints(data_gg["arXiv_2101.01629"],(50,50))
 
+
+data_gg["arXiv_2103.01290_1"] = np.genfromtxt("data/GLUINOGLUINOX/arXiv_2103.01290/HEPData-ins1849522-v1-Figure_10-a_Observed_Lines.csv", delimiter=",", skip_header=505, skip_footer=166, names=["x","y"])
+data_gg["arXiv_2103.01290_1"] = add_zero_endpoints(data_gg["arXiv_2103.01290_1"],(0,0))
+
+data_gg["arXiv_2103.01290_2"] = np.genfromtxt("data/GLUINOGLUINOX/arXiv_2103.01290/HEPData-ins1849522-v1-Figure_10-b_Observed_Lines.csv", delimiter=",", skip_header=475, skip_footer=154, names=["x","y"])
+data_gg["arXiv_2103.01290_2"] = add_zero_endpoints(data_gg["arXiv_2103.01290_2"],(0,0))
+
+data_gg["arXiv_2103.01290_3"] = np.genfromtxt("data/GLUINOGLUINOX/arXiv_2103.01290/HEPData-ins1849522-v1-Figure_11_Observed_Lines.csv", delimiter=",", skip_header=677, skip_footer=223, names=["x","y"])
+data_gg["arXiv_2103.01290_3"] = add_zero_endpoints(data_gg["arXiv_2103.01290_3"],(0,0))
+
+
+
+data_gg["arXiv_2204.13072_1"] = np.genfromtxt("data/GLUINOGLUINOX/arXiv_2204.13072/HEPData-ins2072870-v1-Figure_16a_Observed_Limit.csv", delimiter=",", skip_header=8, skip_footer=0, names=["x","y"])
+data_gg["arXiv_2204.13072_1"] = add_zero_endpoints(data_gg["arXiv_2204.13072_1"],(0,0))
+
+data_gg["arXiv_2204.13072_2"] = np.genfromtxt("data/GLUINOGLUINOX/arXiv_2204.13072/HEPData-ins2072870-v1-Figure_16b_Observed_Limit.csv", delimiter=",", skip_header=8, skip_footer=0, names=["x","y"])
+data_gg["arXiv_2204.13072_2"] = add_zero_endpoints(data_gg["arXiv_2204.13072_2"],(0,0))
+
+data_gg["arXiv_2204.13072_3"] = np.genfromtxt("data/GLUINOGLUINOX/arXiv_2204.13072/HEPData-ins2072870-v1-Figure_16c_Observed_Limit.csv", delimiter=",", skip_header=8, skip_footer=0, names=["x","y"])
+data_gg["arXiv_2204.13072_3"] = add_zero_endpoints(data_gg["arXiv_2204.13072_3"],(0,0))
+
+
+
+
+
+data_gg["arXiv_2206.06012_1"] = np.genfromtxt("data/GLUINOGLUINOX/arXiv_2206.06012/HEPData-ins2094882-v2-Observed_limit_for_gH_model.csv", delimiter=",", skip_header=12, skip_footer=0, names=["x","y"])
+data_gg["arXiv_2206.06012_1"] = add_zero_endpoints(data_gg["arXiv_2206.06012_1"],(0,0))
+
+data_gg["arXiv_2206.06012_2"] = np.genfromtxt("data/GLUINOGLUINOX/arXiv_2206.06012/HEPData-ins2094882-v2-Observed_limit_for_gZ_model.csv", delimiter=",", skip_header=12, skip_footer=0, names=["x","y"])
+data_gg["arXiv_2206.06012_2"] = add_zero_endpoints(data_gg["arXiv_2206.06012_2"],(0,0))
+
+
+
+
+data_gg["arXiv_2211.08028_1"] = np.genfromtxt("data/GLUINOGLUINOX/arXiv_2211.08028/HEPData-ins2182381-v2-Observed_CLs_limit_for_Gbb_from_CC.csv", delimiter=",", skip_header=9, skip_footer=0, names=["x","y"])
+data_gg["arXiv_2211.08028_1"] = add_zero_endpoints(data_gg["arXiv_2211.08028_1"],(0,0))
+
+data_gg["arXiv_2211.08028_2"] = np.genfromtxt("data/GLUINOGLUINOX/arXiv_2211.08028/HEPData-ins2182381-v2-Observed_CLs_limit_for_Gbb_from_NN.csv", delimiter=",", skip_header=9, skip_footer=0, names=["x","y"])
+data_gg["arXiv_2211.08028_2"] = add_zero_endpoints(data_gg["arXiv_2211.08028_2"],(0,0))
+
+data_gg["arXiv_2211.08028_3"] = np.genfromtxt("data/GLUINOGLUINOX/arXiv_2211.08028/HEPData-ins2182381-v2-Observed_CLs_limit_for_Gtt_from_CC.csv", delimiter=",", skip_header=9, skip_footer=0, names=["x","y"])
+data_gg["arXiv_2211.08028_3"] = add_zero_endpoints(data_gg["arXiv_2211.08028_3"],(0,0))
+
+data_gg["arXiv_2211.08028_4"] = np.genfromtxt("data/GLUINOGLUINOX/arXiv_2211.08028/HEPData-ins2182381-v2-Observed_CLs_limit_for_Gtt_from_NN.csv", delimiter=",", skip_header=9, skip_footer=0, names=["x","y"])
+data_gg["arXiv_2211.08028_4"] = add_zero_endpoints(data_gg["arXiv_2211.08028_4"],(0,0))
+
+
+
+data_gg["arXiv_2307.01094_1"] = np.genfromtxt("data/GLUINOGLUINOX/arXiv_2307.01094/HEPData-ins2673888-v1-Exclusion_contour(Obs)_from_Fig_7(a).csv", delimiter=",", skip_header=10, skip_footer=0, names=["x","y"])
+data_gg["arXiv_2307.01094_1"] = add_zero_endpoints(data_gg["arXiv_2307.01094_1"],(0,0))
+
+data_gg["arXiv_2307.01094_2"] = np.genfromtxt("data/GLUINOGLUINOX/arXiv_2307.01094/HEPData-ins2673888-v1-Exclusion_contour(Obs)_from_Fig_7(c).csv", delimiter=",", skip_header=10, skip_footer=0, names=["x","y"])
+data_gg["arXiv_2307.01094_2"] = add_zero_endpoints(data_gg["arXiv_2307.01094_2"],(0,0))
+
+data_gg["arXiv_2307.01094_3"] = np.genfromtxt("data/GLUINOGLUINOX/arXiv_2307.01094/HEPData-ins2673888-v1-Exclusion_contour(Obs)_from_Fig_7(e).csv", delimiter=",", skip_header=10, skip_footer=0, names=["x","y"])
+data_gg["arXiv_2307.01094_3"] = add_zero_endpoints(data_gg["arXiv_2307.01094_3"],(0,0))
+
+data_gg["arXiv_2307.01094_4"] = np.genfromtxt("data/GLUINOGLUINOX/arXiv_2307.01094/HEPData-ins2673888-v1-Exclusion_contour(Obs)_from_Fig_7(f).csv", delimiter=",", skip_header=10, skip_footer=0, names=["x","y"])
+data_gg["arXiv_2307.01094_4"] = add_zero_endpoints(data_gg["arXiv_2307.01094_4"],(0,0))
+
+
+
+
+
+
+
+
+
 print(data_gg)
 
 fig, ax = plt.subplots(1,1)
@@ -80,7 +148,7 @@ def getArraysFromTGraph(tgraph):
 #
 i=0
 
-alpha=0.3
+alpha=2/len(data_gg)#0.3
 
 ax.fill(data_gg["arXiv_1908.04722"]['x'], data_gg["arXiv_1908.04722"]['y'], color=colors[i], alpha=alpha, lw=0)
 
@@ -94,14 +162,47 @@ ax.fill(data_gg["arXiv_2010.14293"]['x'], data_gg["arXiv_2010.14293"]['y'], colo
 ax.fill(data_gg["arXiv_2101.01629"]['x'], data_gg["arXiv_2101.01629"]['y'], color=colors[i], alpha=alpha, lw=0)
 
 
+ax.fill(data_gg["arXiv_2103.01290_1"]['x'], data_gg["arXiv_2103.01290_1"]['y'], color=colors[i], alpha=alpha, lw=0)
+ax.fill(data_gg["arXiv_2103.01290_2"]['x'], data_gg["arXiv_2103.01290_2"]['y'], color=colors[i], alpha=alpha, lw=0)
+ax.fill(data_gg["arXiv_2103.01290_3"]['x'], data_gg["arXiv_2103.01290_3"]['y'], color=colors[i], alpha=alpha, lw=0)
+
+
+ax.fill(data_gg["arXiv_2204.13072_1"]['x'], data_gg["arXiv_2204.13072_1"]['y'], color=colors[i], alpha=alpha, lw=0)
+ax.fill(data_gg["arXiv_2204.13072_2"]['x'], data_gg["arXiv_2204.13072_2"]['y'], color=colors[i], alpha=alpha, lw=0)
+ax.fill(data_gg["arXiv_2204.13072_3"]['x'], data_gg["arXiv_2204.13072_3"]['y'], color=colors[i], alpha=alpha, lw=0)
+
+
+ax.fill(data_gg["arXiv_2206.06012_1"]['x'], data_gg["arXiv_2206.06012_1"]['y'], color=colors[i], alpha=alpha, lw=0)
+ax.fill(data_gg["arXiv_2206.06012_2"]['x'], data_gg["arXiv_2206.06012_2"]['y'], color=colors[i], alpha=alpha, lw=0)
+
+
+
+ax.fill(data_gg["arXiv_2211.08028_1"]['x'], data_gg["arXiv_2211.08028_1"]['y'], color=colors[i], alpha=alpha, lw=0)
+ax.fill(data_gg["arXiv_2211.08028_2"]['x'], data_gg["arXiv_2211.08028_2"]['y'], color=colors[i], alpha=alpha, lw=0)
+ax.fill(data_gg["arXiv_2211.08028_3"]['x'], data_gg["arXiv_2211.08028_3"]['y'], color=colors[i], alpha=alpha, lw=0)
+ax.fill(data_gg["arXiv_2211.08028_4"]['x'], data_gg["arXiv_2211.08028_4"]['y'], color=colors[i], alpha=alpha, lw=0)
+
+
+ax.fill(data_gg["arXiv_2307.01094_1"]['x'], data_gg["arXiv_2307.01094_1"]['y'], color=colors[i], alpha=alpha, lw=0)
+ax.fill(data_gg["arXiv_2307.01094_2"]['x'], data_gg["arXiv_2307.01094_2"]['y'], color=colors[i], alpha=alpha, lw=0)
+ax.fill(data_gg["arXiv_2307.01094_3"]['x'], data_gg["arXiv_2307.01094_3"]['y'], color=colors[i], alpha=alpha, lw=0)
+ax.fill(data_gg["arXiv_2307.01094_4"]['x'], data_gg["arXiv_2307.01094_4"]['y'], color=colors[i], alpha=alpha, lw=0)
+
+
+
+
+
+
+
+
 ax.set_xlabel(r'$m_{X}$ [GeV]',)
 ax.set_ylabel(r'$m_{\chi^0_1}$ [GeV]',)
 # ax.xaxis.set_label_coords(1.02, -0.07)
 # ax.set_ylabel(r'Excluded Stop Squark Mass $m_{\tilde{t}}$ [GeV]')
 # ax.set_xlim([2e-6,2e4])
 # ax2.set_xlim([1.1e13,9e18])
-ax.set_ylim([0,1500])
-ax.set_xlim([0,2500])
+ax.set_ylim([0,2500])
+ax.set_xlim([0,2700])
 # plt.grid()
 
 
