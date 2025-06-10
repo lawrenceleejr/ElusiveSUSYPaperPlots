e3f30afc74f26f54a0fbce57c9fe4eb135c52808
Lawrence Lee <lawrenceleejr@gmail.com>
2025-06-10 11:40:33 -0700

    dressed up RPV plot.

--- plot_rpvModelDep/plot.py
+++ plot_rpvModelDep/plot.py
@@ -10,15 +10,16 @@ import csv
 from array import array
 
 # colors = sns.color_palette("husl", 3)
-colors = ["#FF595E",  "#1982C4", "#8AC926",] 
-# colors = ["#E07A5F",  # Terra Cotta
-# 		"#F2CC8F",  # Sand
-# 		"#81B29A"]  # Sage
+# colors = ["#FF595E",  "#1982C4", "#8AC926",] 
+colors = ["#E07A5F",  # Terra Cotta
+		"#81B29A",
+		"#F2CC8F",  # Sand
+        ]  # Sage
 
 # colors = ["#FF0000", "#00FF00", "#0000FF"]
 # colors = ["#FF6B6B", "#6BCB77", "#4D96FF"]
-# colors = ["#3A86FF", "#8338EC", "#FB5607"]
-# colors = ["#FF6B6B", "#4ECDC4", "#1A535C"]
+# colors = ["#3A86FF",  "#FB5607","#8338EC",]
+# colors = ["#FF6B6B","#1A535C", "#4ECDC4", ]
 # colors = ["#4477AA", "#CC6677", "#117733"]
 
 def add_zero_endpoints(arr, point=(0,0)):
@@ -40,7 +41,7 @@ def add_zero_endpoints(arr, point=(0,0)):
 
 
 baselength=4
-fig, ax = plt.subplots(1,1, figsize=(1.5*baselength, 1*baselength))
+fig, ax = plt.subplots(2,1, figsize=(1.5*baselength, 2*baselength))
 
 
 data = {}
@@ -55,7 +56,7 @@ data["multib_trunc"] = data["multib"][data['multib']['x'] <= 4e-2]
 
 
 
-def doFillBetween(x,y,n=10,dy=1,color="k",alpha=0.03,log=True,axis=ax):
+def doFillBetween(x,y,n=10,dy=1,color="k",alpha=0.03,log=True,axis=None):
 	initialY = y
 	tmpy = initialY
 
@@ -125,11 +126,21 @@ def arrMassFromXS(arr):
       return [getMassFromXS(x) for x in arr]
 
 
-ax.plot( np.log10(data["rpv1l"]["x"]), data["rpv1l"]["y"], lw=3 , c=colors[0], zorder=10)
-ax.plot( np.log10(data["multib"]["x"]), data["multib"]["y"], lw=3 , c=colors[1],zorder=10)
 
 
-# if split evenly with 223, 123
+ax[0].plot( np.log10(data["rpv1l"]["x"]), data["rpv1l"]["y"], lw=2 , c=colors[0], zorder=0)
+ax[0].plot( np.log10(data["multib"]["x"]), data["multib"]["y"], lw=2 , c=colors[1],zorder=0)
+
+
+
+ax[1].plot( np.log10(data["rpv1l"]["x"]), data["rpv1l"]["y"], lw=2 , c=colors[0], zorder=0)
+ax[1].plot( np.log10(data["multib"]["x"]), data["multib"]["y"], lw=2 , c=colors[1],zorder=0)
+
+
+
+
+
+# if split evenly with 112
 # it will double the cross section limit (make it weaker)
 
 reductionfactor=2
@@ -138,20 +149,75 @@ reductionfactor=2
 tmpx = np.log10([x/(reductionfactor) for x in data["rpv1l_trunc"]["x"]])
 tmpy = [x*reductionfactor*reductionfactor for x in arrXSFromMass(data["rpv1l_trunc"]["y"])]
 tmpy = arrMassFromXS(tmpy)
-ax.plot( tmpx, tmpy, "--", lw=1.5 , c=colors[0])
+ax[0].plot( tmpx, tmpy, "--", lw=1.5 , c=colors[0])
+
+doFillBetween(tmpx, tmpy, axis=ax[0], dy=0.99, alpha=0.2, n=50, color=colors[0])
+
 
 # But on the right side, you don't get a shift since it's all prompt. You do get that same factor 4 reduction in the BR.
 newMass = getMassFromXS(getXSFromMass(data["rpv1l"]["y"][-1])*4)
-ax.plot( 
+ax[0].plot( 
       [tmpx[-1],data["rpv1l"]["x"][-1]], 
       [tmpy[-1],newMass], 
-      ":", lw=1.5 , c=colors[0])
+      ":", lw=1.5 , c="k", alpha=0.3)
 
 # Multi-b on the other hand... it mostly has sensitivity on the left from the chi escapes. So it's affected by the change in lifetime. But since the b's are coming from the prompt Gtt tops, there's no reduction in the upper limit.
 tmpx = np.log10([x/reductionfactor for x in data["multib_trunc"]["x"]])
 tmpy = [x for x in arrXSFromMass(data["multib_trunc"]["y"])]
 tmpy = arrMassFromXS(tmpy)
-ax.plot( tmpx, tmpy, "--",lw=1.5 , c=colors[1])
+ax[0].plot( tmpx, tmpy, "--",lw=1.5 , c=colors[1])
+
+doFillBetween(tmpx, tmpy, axis=ax[0], dy=0.99, alpha=0.2, n=50, color=colors[1])
+
+newMass = getMassFromXS(getXSFromMass(data["multib"]["y"][-1])*4)
+ax[0].plot( 
+      [tmpx[-1],data["multib"]["x"][-1]], 
+      [tmpy[-1],newMass], 
+      ":", lw=1.5 , c="k", alpha=0.3)
+
+
+
+
+
+# if this only represents 10% of the total width...
+reductionfactor=10
+
+# Since RPV1L cares about getting a prompt lepton, and doesn't care about MET, as soon as there's another component to the chi decay, it loses sensitivity. Since there are two decays, this is reduced by a factor of 4. Since it now has two decay modes, the lifetime is halved.
+tmpx = np.log10([x/(reductionfactor) for x in data["rpv1l_trunc"]["x"]])
+tmpy = [x*reductionfactor*reductionfactor for x in arrXSFromMass(data["rpv1l_trunc"]["y"])]
+tmpy = arrMassFromXS(tmpy)
+ax[1].plot( tmpx, tmpy, "--", lw=1.5 , c=colors[0])
+
+doFillBetween(tmpx, tmpy, axis=ax[1], dy=0.99, alpha=0.2, n=50, color=colors[0])
+
+
+# But on the right side, you don't get a shift since it's all prompt. You do get that same factor 4 reduction in the BR.
+newMass = getMassFromXS(getXSFromMass(data["rpv1l"]["y"][-1])*100)
+ax[1].plot( 
+      [tmpx[-1],data["rpv1l"]["x"][-1]], 
+      [tmpy[-1],newMass], 
+      ":", lw=1.5 , c="k", alpha=0.3)
+
+# Multi-b on the other hand... it mostly has sensitivity on the left from the chi escapes. So it's affected by the change in lifetime. But since the b's are coming from the prompt Gtt tops, there's no reduction in the upper limit.
+tmpx = np.log10([x/reductionfactor for x in data["multib_trunc"]["x"]])
+tmpy = [x for x in arrXSFromMass(data["multib_trunc"]["y"])]
+tmpy = arrMassFromXS(tmpy)
+ax[1].plot( tmpx, tmpy, "--",lw=1.5 , c=colors[1])
+
+doFillBetween(tmpx, tmpy, axis=ax[1], dy=0.99, alpha=0.2, n=50, color=colors[1])
+
+
+newMass = getMassFromXS(getXSFromMass(data["multib"]["y"][-1])*100)
+ax[1].plot( 
+      [tmpx[-1],data["multib"]["x"][-1]], 
+      [tmpy[-1],newMass], 
+      ":", lw=1.5 , c="k", alpha=0.3)
+
+
+
+
+
+
 
 # And when you turn on 112, you start to get limits from 0L. This similarly uses the chi escaping the detector, so only the lifetime change is relevant.
 # But the problem is in that analysis, the 
@@ -159,7 +225,7 @@ ax.plot( tmpx, tmpy, "--",lw=1.5 , c=colors[1])
 # tmpx = np.log10([x/reductionfactor for x in data["0l"]["x"]])
 # tmpy = [x for x in arrXSFromMass(data["0l"]["y"])]
 # tmpy = arrMassFromXS(tmpy)
-# ax.plot( tmpx, tmpy, "--",lw=1.5 , c=colors[2])
+# ax[0].plot( tmpx, tmpy, "--",lw=1.5 , c=colors[2])
 
 
 
@@ -171,49 +237,67 @@ ax.plot( tmpx, tmpy, "--",lw=1.5 , c=colors[1])
 
 
 
-ax.set_xlabel(r'$\lambda^{\prime\prime}_{323}$',)
-ax.set_ylabel(r'$m(\tilde{g})$ [GeV]',)
-ax.set_xlim([-3.5,0])
-ax.set_ylim([1000,2500])
+ax[0].set_xlabel(r'$\lambda^{\prime\prime}_{323}$',)
+ax[0].set_ylabel(r'$m(\tilde{g})$ [GeV]',)
+ax[0].set_xlim([-3.5,0])
+ax[0].set_ylim([1000,2400])
+
+ax[1].set_xlabel(r'$\lambda^{\prime\prime}_{323}$',)
+ax[1].set_ylabel(r'$m(\tilde{g})$ [GeV]',)
+ax[1].set_xlim([-3.5,0])
+ax[1].set_ylim([1000,2400])
+
+ax[0].spines['bottom'].set_visible(False)
+ax[0].xaxis.set_ticks_position('none')  # Optional: hide the ticks
+ax[0].tick_params(bottom=False)     
+ax[0].set_xticklabels([])
+ax[0].set_xlabel("")
 
 
-# ax.set_xlabel(r'$\lambda^{\prime\prime}_{323}=\lambda^{\prime\prime}_{112}$',)
-# ax.set_ylabel(r'$m(\tilde{g})$ [GeV]',)
-# ax.set_xlim([-3.2,0])
-# ax.set_ylim([1000,2500])
+# ax[0].set_xlabel(r'$\lambda^{\prime\prime}_{323}=\lambda^{\prime\prime}_{112}$',)
+# ax[0].set_ylabel(r'$m(\tilde{g})$ [GeV]',)
+# ax[0].set_xlim([-3.2,0])
+# ax[0].set_ylim([1000,2500])
 
 
-# ax.set_xscale('log')
+# ax[0].set_xscale('log')
 
 
 
-ax.text(200, 2050,       r"Sparticle Limits, Strong Production", size=11,clip_on=False, fontweight="bold")
-ax.text(200, 2050-1*150, r"Various Assumptions", size=11,clip_on=False)
-ax.text(200, 2050-2*150, r"Run-2 LHC", size=11,clip_on=False)
-
-ax.text(1700, 1000, r"Gluinos", size=11,clip_on=False, color="k", alpha=0.6, fontweight='bold')
-ax.text(1000, 500, r"Squarks", size=11,clip_on=False,  color="k", alpha=0.6, fontweight='bold')
+ax[0].text(-0., 2400,       r"Limits on RPV SUSY, ATLAS", size=11,clip_on=False, ha="right", fontweight="bold")
+ax[0].text(-0., 2400-1*80, r"RPV Model Dependence", size=11,clip_on=False, ha="right")
 
 
-# breathe(ax)
+ax[0].text(-3.45, 1200, r"$\Gamma_{\tilde{\chi}^0_1}\  (\lambda^{\prime\prime}_{323})=50\%$", size=13,clip_on=False,  color="k", alpha=1, fontweight='bold')
 
-fig.subplots_adjust(left=0.125, right=0.96, bottom=0.15, top=0.98)
+ax[1].text(-3.45, 1400, r"$\Gamma_{\tilde{\chi}^0_1}\  (\lambda^{\prime\prime}_{323})=10\%$", size=13,clip_on=False,  color="k", alpha=1, fontweight='bold')
+
+
+
+ax[1].text(-2.0, 1800, r"$\Gamma_{\tilde{\chi}^0_1}\  (\lambda^{\prime\prime}_{323})=100\%$", size=13,clip_on=False,  color="k", alpha=1, fontweight='bold')
+
+ax[0].text(-0.5, 1450, r"$≥3$ b-jets", size=11,clip_on=False,  color="k", alpha=1, fontweight='bold')
+ax[0].text(-0.5, 2100, r"$1\ell$+jets", size=11,clip_on=False,  color="k", alpha=1, fontweight='bold')
+
+
+
+breathe(ax[0])
+breathe(ax[1])
+
+fig.subplots_adjust(left=0.15, right=0.96, bottom=0.15, top=0.98)
 # Force figure to render, so transforms are accurate
 fig.canvas.draw()
 
-# Transform from data to display coordinates
-p0 = ax.transData.transform((0, 0))
-p1 = ax.transData.transform((1, 1))
-
-# Compute angle in screen/display space
-dx, dy = p1 - p0
-angle_rad = np.arctan2(dy, dx)
-angle_deg = np.degrees(angle_rad)
-
-# ax.text(100, 160, r"$m_{\tilde{\chi}^0_1}>m_X$", size=9,clip_on=False, rotation=angle_deg, ha='left', va='bottom')
-# ax.plot( [0,2500], [0,2500], "--", lw=0.5, color="black" )
-
-
+ax[1].set_xticklabels([
+      r"$10^{-3.5}$",
+      r"$10^{-3}$",
+      r"$10^{-2.5}$",
+      r"$10^{-2}$",
+      r"$10^{-1.5}$",
+      r"$10^{-1}$",
+      r"$10^{-0.5}$",
+      r"$10^{-0}$",
+])
 
 fig.savefig("RPVModelDep.pdf")
 # plt.show()
